3e3e122b981a1993239abbbafe3b25c441bfa645
Shivam Sharma <shivam7.dev@gmail.com>
2024-05-26 00:12:21 +0530

    added the serach component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
 import appDownload from "../assets/appDownload.png";
 import landing from "../assets/landing.png";
+import { useNavigate } from "react-router-dom";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className=" flex flex-col gap-12">
       <div className=" bg-white rounded-1g shadow-md py-8 flex-col gap-5 text-center -mt-16">
@@ -8,6 +19,10 @@ export default function HomePage() {
           Tuck into takeway today
         </h1>
         <span className=" text-xl">Food is just a clik away!</span>
+        <SearchBar
+          placeHolder="seach by city or town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className=" grid md:grid-cols-2 gap-5">
         <img src={landing} alt="" />
