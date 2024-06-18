89e54468ec2c5063e01d671d53c54458f6c825d4
Shivam Sharma <shivam7.dev@gmail.com>
2024-06-18 23:33:00 +0530

    added the form data

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -16,8 +16,8 @@ export default function Auth0ProviderWithNavigate({ children }: PropsType) {
   if (!domain || !clientID || !redirectURI || !audience) {
     throw new Error("unable to reach the Auth0");
   }
-  const onRedirectCallback = () => {
-    navigate("/auth-callback");
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || "/auth-callback");
   };
   return (
     <Auth0Provider
--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,5 +1,5 @@
-import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
@@ -19,25 +19,29 @@ import { useEffect } from "react";
 const formSchema = z.object({
   email: z.string().optional(),
   name: z.string().min(1, "name is required"),
-  addressLine1: z.string().min(1, "Address line 1 is required"),
-  city: z.string().min(1, "city is required"),
-  country: z.string().min(1, "country is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
 });
 
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
+  currentUser: User;
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
-  currentUser: User;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ isLoading, onSave, currentUser }: Props) => {
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "User Profile",
+  buttonText = "Submit",
+}: Props) => {
   const form = useForm<UserFormData>({
-    /**
-     *  resolver is used from validations and all
-     * formSchema is place where we  define validations and validation messages
-     */
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
   });
@@ -53,73 +57,73 @@ const UserProfileForm = ({ isLoading, onSave, currentUser }: Props) => {
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className=" text-2xl font-bold">User Profile Form</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <FormDescription>
             View and change your profile information here
           </FormDescription>
         </div>
-
         <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
-            <FormItem className=" flex-1">
-              <FormLabel> Email</FormLabel>
+            <FormItem>
+              <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input {...field} disabled className=" bg-white" />
+                <Input {...field} disabled className="bg-white" />
               </FormControl>
             </FormItem>
           )}
         />
+
         <FormField
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem className=" flex-1">
-              <FormLabel> Name</FormLabel>
+            <FormItem>
+              <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input {...field} className=" bg-white" />
+                <Input {...field} className="bg-white" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        <div className=" flex flex-col md:flex-row gap 4 ">
+        <div className="flex flex-col md:flex-row gap-4">
           <FormField
             control={form.control}
             name="addressLine1"
             render={({ field }) => (
-              <FormItem className=" flex-1">
-                <FormLabel> Address line 1</FormLabel>
+              <FormItem className="flex-1">
+                <FormLabel>Address Line 1</FormLabel>
                 <FormControl>
-                  <Input {...field} className=" bg-white" />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
-          />{" "}
+          />
           <FormField
             control={form.control}
             name="city"
             render={({ field }) => (
-              <FormItem className=" flex-1">
-                <FormLabel> City</FormLabel>
+              <FormItem className="flex-1">
+                <FormLabel>City</FormLabel>
                 <FormControl>
-                  <Input {...field} className=" bg-white" />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
-          />{" "}
+          />
           <FormField
             control={form.control}
             name="country"
             render={({ field }) => (
-              <FormItem className=" flex-1">
-                <FormLabel> Country</FormLabel>
+              <FormItem className="flex-1">
+                <FormLabel>Country</FormLabel>
                 <FormControl>
-                  <Input {...field} className=" bg-white" />
+                  <Input {...field} className="bg-white" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -129,9 +133,8 @@ const UserProfileForm = ({ isLoading, onSave, currentUser }: Props) => {
         {isLoading ? (
           <LoadingButton />
         ) : (
-          <Button type="submit" className=" bg-orange-500">
-            {" "}
-            Submit
+          <Button type="submit" className="bg-orange-500">
+            {buttonText}
           </Button>
         )}
       </form>
